9aef2b2db760b826f1c6ba959cef2b620dcc6933
AlbertBertilsson <abbe_something@hotmail.com>
2015-10-29 12:05:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,21 @@
 <?php
 
-date_default_timezone_set("UTC");
+function log() {
 
-function trello_log() {
+  $logurl = "https://logs-01.loggly.com/inputs/" . getenv("loggly-token") . "/tag/trellotest/";
 
-  $server = $_SERVER;
-  $server['trello-key'] = "<key>";
-  $server['trello-token'] = "<token>";
-
-  $trellologurl = "https://api.trello.com/1/cards/?token=" . getenv("trello-token") .
-  	"&key=" . getenv("trello-key") .
-  	"&due=null".
-  	"&urlSource=null" .
-  	"&idList=" . getenv("trello-log") .
-  	"&name=" . urlencode(date("Y-m-d g:i:s")) .
-  	"&desc=" . urlencode(var_export($server, true)) . "%0a%0a" . urlencode(file_get_contents('php://input'));
-
-  $ch = curl_init($trellologurl);
+  $ch = curl_init($logurl);
 
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+  curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents('php://input');
 
   $atresult = curl_exec($ch);
 
   if(curl_errno($ch)) {
-    echo "Failed to create log card! Curl error: " . curl_error($ch);
+    echo "Failed to log! Curl error: " . curl_error($ch);
   }
   curl_close ($ch);
 }
 
-trello_log();
+log();
