8aa6dbdd005d5ef6e1b2f0329ba61ce1abb60ba2
AlbertBertilsson <abbe_something@hotmail.com>
2015-10-30 08:55:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,4 @@
 <?php
-function loggly_log() {
+require_once('log.php');
 
-  $logurl = "https://logs-01.loggly.com/inputs/" . getenv("loggly-token") . "/tag/trellotest/";
-
-  $ch = curl_init($logurl);
-
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-  curl_setopt($ch, CURLOPT_POSTFIELDS, file_get_contents('php://input') );
-
-  $atresult = curl_exec($ch);
-
-  if(curl_errno($ch)) {
-    echo "Failed to log! Curl error: " . curl_error($ch);
-  }
-
-  curl_close ($ch);
-}
-
-loggly_log();
+loggly_log(file_get_contents('php://input'), "trellotest");
