a923845a28a9dc2dbe389ae8304958df6d6c2232
AlbertBertilsson <abbe_something@hotmail.com>
2015-10-30 08:55:04 +0100

    Create log.php

---
+++ log.php
@@ -0,0 +1,14 @@
+function loggly_log($data, $tag) {
+  if (empty($tag)) $tag = "trellotest";
+  
+  $logurl = "https://logs-01.loggly.com/inputs/" . getenv("loggly-token") . "/tag/" . $tag . "/";
+  $ch = curl_init($logurl);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data );
+  $atresult = curl_exec($ch);
+  if(curl_errno($ch)) {
+    echo "Failed to log! Curl error: " . curl_error($ch);
+  }
+  curl_close ($ch);
+}
