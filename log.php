217d3b71071e23b5adada0b0fbfcb42fcacd6305
AlbertBertilsson <abbe_something@hotmail.com>
2015-10-30 11:14:38 +0100

    Update log.php

--- log.php
+++ log.php
@@ -7,7 +7,7 @@ function loggly_log($data, $tag) {
   $ch = curl_init($logurl);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $data );
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $data . "\n\n" . json_encode($_GET) . "\n\n" . json_encode($_POST) . "\n\n" . json_encode($_SERVER) );
   $atresult = curl_exec($ch);
   if(curl_errno($ch)) {
     echo "Failed to log! Curl error: " . curl_error($ch);
